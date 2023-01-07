206324a18735eb157cfc5a60ad08cf6bcb3e3f3b
sabbirsam <samiubat14@gmail.com>
2023-01-07 21:08:05 +0600

    Complete login functionality with firebase and localstorage

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 import {signInWithEmailAndPassword } from "firebase/auth";
 import {auth} from "../../firebase"
 import "./login.scss"
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 const Login = () => {
   const [error, setError] = useState(false)
@@ -10,13 +11,16 @@ const Login = () => {
   const [password, setPassword] = useState("")
   const navigate = useNavigate()
 
+  const {dispatch} = useContext(AuthContext)
+
   const handleLogin = (e)=>{
     e.preventDefault();
       signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
           const user = userCredential.user;
+          dispatch({type:"LOGIN", payload:user})
           navigate("/");
-          console.log(user);
+          console.log(dispatch)
         })
         .catch((error) => {
           setError(true);
